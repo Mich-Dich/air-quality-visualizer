d07d8a4e393354602149743f73d3c6b0de09bc51
Fabian_Laptop <fb-224651@hs-weingarten.de>
2024-01-18 17:24:38 +0100

    Update Navbar.vue styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,19 +7,23 @@ a {
 a:focus {
   color: inherit;
 }
+
+.title {
+  /* text-shadow: 0 0 10px rgba(255, 255, 255, 0.5); */
+}
 </style>
 
 <template>
   <nav>
-    <v-toolbar app>
+    <v-toolbar app color="rgb(31, 31, 31)">
       <v-toolbar-title class="text-uppercase">
         <span class="font-weight-light"> Airquality </span>
-        <span class="font-weight-bold"> Visualizer </span>
+        <span class="font-weight-bold text-primary title"> Visualizer </span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn>
         <span class="font-weight-bold">Mode</span>
-        <v-icon right>mdi mdi-dark mdi-invert-colors</v-icon>
+        <v-icon right>mdi mdi-invert-colors</v-icon>
       </v-btn>
       <v-btn>
         <a href="https://github.com" target="_blank" rel="noopener noreferrer">
