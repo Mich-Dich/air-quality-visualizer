d9be1a709cad948fbdd3c54ed1e22bb93d9dfe3b
Fabian_PC <fb-224651@hs-weingarten.de>
2024-01-18 13:56:57 +0100

    Implement tabs for options display
    -the tabs will show the different input fields depending on the view
     and the configuration that are available for that view
    
    -implemented first tab "Luftqualität" and save all unused inputs in
     its own temporary tab

--- src/components/OptionsDisplay.vue
+++ src/components/OptionsDisplay.vue
@@ -3,107 +3,207 @@
     class="pa-3"
     :title="title"
     :subtitle="subtitle"
-    width="550px"
+    max-width="550px"
     height="auto"
     color="rgb(31, 31, 31)"
   >
-    <VueDatePicker
-      v-model="startDate"
-      placeholder="Start Date"
-      dark
-      class="mb-5"
-      position="right"
-      time-picker-inline
-      auto-apply
-      partial-flow
-      min-date="1 / 1 / 2016"
-      :max-date="new Date()"
-      prevent-min-max-navigation
-      locale="de"
-    ></VueDatePicker>
+    <v-tabs v-model="tab" bg-color="primary" class="mb-3" fixed-tabs>
+      <v-tab value="one">Luftqualität</v-tab>
+      <v-tab value="two">Messwerte</v-tab>
+      <v-tab value="three">Alle Filter</v-tab></v-tabs
+    >
 
-    <VueDatePicker
-      v-model="endDate"
-      placeholder="End Date"
-      dark
-      class="mb-5"
-      position="right"
-      time-picker-inline
-      auto-apply
-      partial-flow
-      min-date="1 / 1 / 2016"
-      :max-date="new Date()"
-      prevent-min-max-navigation
-      locale="de"
-    ></VueDatePicker>
+    <v-window v-model="tab">
+      <!-- Ansicht Luftqualität -->
+      <v-window-item value="one">
+        <v-row>
+          <v-col cols="6">
+            <VueDatePicker
+              v-model="Date"
+              placeholder="Datum"
+              dark
+              class="mb-5"
+              :enable-time-picker="false"
+              auto-apply
+              min-date="1 / 1 / 2016"
+              :max-date="new Date()"
+              prevent-min-max-navigation
+              locale="de"
+            ></VueDatePicker>
+          </v-col>
 
-    <v-autocomplete
-      v-model="selectedComponent"
-      variant="outlined"
-      clearable
-      label="Schadstoff"
-      :items="componentNames"
-    ></v-autocomplete>
+          <v-col cols="6">
+            <VueDatePicker
+              v-model="Time"
+              placeholder="Uhrzeit"
+              dark
+              class="mb-5"
+              time-picker
+              auto-apply
+              locale="de"
+            >
+              <template #input-icon>
+                <img
+                  class="input-slot-image"
+                  src="./icons/clock.png"
+                /> </template
+            ></VueDatePicker>
+          </v-col>
+        </v-row>
 
-    <v-autocomplete
-      v-model="selectedTransgression"
-      variant="outlined"
-      clearable
-      label="Überschreitungswert"
-      :items="transgressionNames"
-    ></v-autocomplete>
+        <v-autocomplete
+          v-model="selectedNetwork"
+          variant="outlined"
+          clearable
+          label="Bundesländer"
+          :items="networkNames"
+        ></v-autocomplete>
 
-    <v-autocomplete
-      v-model="selectedScope"
-      variant="outlined"
-      clearable
-      label="Zeitbezug"
-      :items="scopeNames"
-    ></v-autocomplete>
+        <v-autocomplete
+          v-model="selectedStation"
+          variant="outlined"
+          clearable
+          label="Stationen"
+          :items="stationNames"
+        ></v-autocomplete>
 
-    <v-autocomplete
-      v-model="selectedStation"
-      variant="outlined"
-      clearable
-      label="Stationen"
-      :items="stationNames"
-    ></v-autocomplete>
+        <v-container class="pa-0 mb-3">
+          <h2 class="text-subtitle-2">
+            Filter Stationen nach geographischer Lage
+          </h2>
+          <v-chip-group v-model="selectedStationSettings" multiple filter>
+            <v-chip
+              v-for="stationSettingName in stationSettingNames"
+              :key="stationSettingName"
+              filter
+              variant="elevated"
+            >
+              {{ stationSettingName }}
+            </v-chip>
+          </v-chip-group>
+        </v-container>
 
-    <v-autocomplete
-      v-model="selectedNetwork"
-      variant="outlined"
-      clearable
-      label="Bundesländer"
-      :items="networkNames"
-    ></v-autocomplete>
+        <v-container class="pa-0 mb-3">
+          <h2 class="text-subtitle-2">Filter Stationen nach Einsatzgebiet</h2>
+          <v-chip-group v-model="selectedStationTypes" multiple filter>
+            <v-chip
+              v-for="stationTypeName in stationTypeNames"
+              :key="stationTypeName"
+              filter
+              variant="elevated"
+            >
+              {{ stationTypeName }}
+            </v-chip>
+          </v-chip-group>
+        </v-container>
+      </v-window-item>
 
-    <v-container class="pa-0 mb-3">
-      <h2 class="text-subtitle-2">Filter Stationen nach geographischer Lage</h2>
-      <v-chip-group v-model="selectedStationSettings" multiple filter>
-        <v-chip
-          v-for="stationSettingName in stationSettingNames"
-          :key="stationSettingName"
-          filter
-          variant="elevated"
-        >
-          {{ stationSettingName }}
-        </v-chip>
-      </v-chip-group>
-    </v-container>
+      <!-- Ansicht Messwerte -->
+      <v-window-item value="two"></v-window-item>
 
-    <v-container class="pa-0 mb-3">
-      <h2 class="text-subtitle-2">Filter Stationen nach Einsatzgebiet</h2>
-      <v-chip-group v-model="selectedStationTypes" multiple filter>
-        <v-chip
-          v-for="stationTypeName in stationTypeNames"
-          :key="stationTypeName"
-          filter
-          variant="elevated"
-        >
-          {{ stationTypeName }}
-        </v-chip>
-      </v-chip-group>
-    </v-container>
+      <!-- Ansicht Alle Filter -->
+      <v-window-item value="three"
+        ><VueDatePicker
+          v-model="startDate"
+          placeholder="Start Date"
+          dark
+          class="mb-5"
+          position="right"
+          time-picker-inline
+          auto-apply
+          partial-flow
+          min-date="1 / 1 / 2016"
+          :max-date="new Date()"
+          prevent-min-max-navigation
+          locale="de"
+        ></VueDatePicker>
+
+        <VueDatePicker
+          v-model="endDate"
+          placeholder="End Date"
+          dark
+          class="mb-5"
+          position="right"
+          time-picker-inline
+          auto-apply
+          partial-flow
+          min-date="1 / 1 / 2016"
+          :max-date="new Date()"
+          prevent-min-max-navigation
+          locale="de"
+        ></VueDatePicker>
+
+        <v-autocomplete
+          v-model="selectedComponent"
+          variant="outlined"
+          clearable
+          label="Schadstoff"
+          :items="componentNames"
+        ></v-autocomplete>
+
+        <v-autocomplete
+          v-model="selectedTransgression"
+          variant="outlined"
+          clearable
+          label="Überschreitungswert"
+          :items="transgressionNames"
+        ></v-autocomplete>
+
+        <v-autocomplete
+          v-model="selectedScope"
+          variant="outlined"
+          clearable
+          label="Zeitbezug"
+          :items="scopeNames"
+        ></v-autocomplete>
+
+        <v-autocomplete
+          v-model="selectedStation"
+          variant="outlined"
+          clearable
+          label="Stationen"
+          :items="stationNames"
+        ></v-autocomplete>
+
+        <v-autocomplete
+          v-model="selectedNetwork"
+          variant="outlined"
+          clearable
+          label="Bundesländer"
+          :items="networkNames"
+        ></v-autocomplete>
+
+        <v-container class="pa-0 mb-3">
+          <h2 class="text-subtitle-2">
+            Filter Stationen nach geographischer Lage
+          </h2>
+          <v-chip-group v-model="selectedStationSettings" multiple filter>
+            <v-chip
+              v-for="stationSettingName in stationSettingNames"
+              :key="stationSettingName"
+              filter
+              variant="elevated"
+            >
+              {{ stationSettingName }}
+            </v-chip>
+          </v-chip-group>
+        </v-container>
+
+        <v-container class="pa-0 mb-3">
+          <h2 class="text-subtitle-2">Filter Stationen nach Einsatzgebiet</h2>
+          <v-chip-group v-model="selectedStationTypes" multiple filter>
+            <v-chip
+              v-for="stationTypeName in stationTypeNames"
+              :key="stationTypeName"
+              filter
+              variant="elevated"
+            >
+              {{ stationTypeName }}
+            </v-chip>
+          </v-chip-group>
+        </v-container>
+      </v-window-item>
+    </v-window>
   </v-card>
 </template>
 
@@ -168,11 +268,22 @@ export default {
       stationSettingNames: [],
       selectedStationTypes: [],
       stationTypeNames: [],
+      Date: null,
+      Time: null,
       startDate: null,
       endDate: null,
+      tab: null,
     };
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.input-slot-image {
+  height: 20px;
+  opacity: 0.5;
+  width: auto;
+  margin-left: 5px;
+  margin-top: 5px;
+}
+</style>
