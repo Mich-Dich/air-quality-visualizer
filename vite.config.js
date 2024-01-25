d8ca2443d67599c61c7cfffc2f46127f32b1312c
Fabian_PC <fb-224651@hs-weingarten.de>
2024-01-25 12:46:16 +0100

    Add base URL for air quality visualizer

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/air-quality-visualizer/",
   plugins: [
     Vue({
       template: { transformAssetUrls },
