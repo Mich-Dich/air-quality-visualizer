b4bfecc53db805164504d01ba0dbba3712c24e0c
Mich <mally.dominik@gmx.de>
2024-01-28 21:09:50 +0100

    attempt to remove backend (curretnls still CORS issues)

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,14 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 const app = express();
 
 app.use((req, res, next) => {
+
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.use(
-  "/proxy",
-  createProxyMiddleware({
-    target: "https://umweltbundesamt.api.proxy.bund.dev",
-    changeOrigin: true,
-    pathRewrite: {
+app.use("/proxy", createProxyMiddleware({
+
+    target: "https://umweltbundesamt.api.proxy.bund.dev", changeOrigin: true, pathRewrite: {
       "^/proxy": "", // remove base path
     },
   })
--- src/services/UmweltbundesamtService.js
+++ src/services/UmweltbundesamtService.js
@@ -4,8 +4,9 @@ import { inferRuntimeType } from "vue/compiler-sfc";
 class UmweltbundesamtService {
   constructor() {
     // Create an axios instance called api that uses the proxy on localhost:3000 to avoid CORS issues
+    
     this.api = axios.create({
-      baseURL: "http://localhost:3000/proxy/api/air_data/v2",
+      baseURL: "https://www.umweltbundesamt.de/api/air_data/v2",
     });
 
     // Store meta data that will be used as parameters for the API calls
@@ -80,89 +81,251 @@ class UmweltbundesamtService {
     console.log("stations [MESSSTATIONEN]", this.stations);
     console.log("components [SCHADSTOFFE]", this.components);
     console.log("networks [BUNDESLÄNDER]", this.networks);
-    console.log(
-      "scopes [ZEITBEZUG DES MESSWERTS Z.B. Acht-Stunden-Mittelwert]",
-      this.scopes
-    );
-    console.log(
-      "stationSettings [GEOGRAPHISCHE Z.B. ländlich oder städtisch]",
-      this.stationSettings
-    );
-    console.log(
-      "stationsTypes [HINTERGRUND, INDUSTRIE, VERKEHR]",
-      this.stationsTypes
-    );
-    console.log(
-      "transgressionTypes [ÜBERSCHREITUNGSWERTE Z.B. 'Tagesmittelwerte über 50 µg/m³']",
-      this.transgressionTypes
-    );
+    console.log("scopes [ZEITBEZUG DES MESSWERTS Z.B. Acht-Stunden-Mittelwert]",this.scopes);
+    console.log("stationSettings [GEOGRAPHISCHE Z.B. ländlich oder städtisch]",this.stationSettings);
+    console.log("stationsTypes [HINTERGRUND, INDUSTRIE, VERKEHR]",this.stationsTypes);
+    console.log("transgressionTypes [ÜBERSCHREITUNGSWERTE Z.B. 'Tagesmittelwerte über 50 µg/m³']",this.transgressionTypes);
   }
 
   // Thes methods are used to get the data from the API endpoints
-  getStations(lang, index) {
-    return this.api.get(`/stations/json?lang=${lang}&index=${index}`);
+  async getStations(lang, index) {
+    // return this.api.get(`/stations/json?lang=${lang}&index=${index}`);
+
+    try {
+      const response = await this.api.get(`/stations/json?lang=${lang}&index=${index}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+      throw error;
+    }
+
   }
 
-  getComponents(lang, code) {
-    return this.api.get(`/components/json?lang=${lang}&index=${code}`);
+  async getComponents(lang, code) {
+
+    try {
+      const response = await this.api.get(`/components/json?lang=${lang}&index=${code}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+      throw error;
+    }
+
+    //return this.api.get(`components/json?lang=${lang}&index=${code}`);
   }
 
-  getNetworks(lang, index) {
+  async getNetworks(lang, index) {
+
+    try {
+      const response = await this.api.get(`/networks/json?lang=${lang}&index=${index}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+      throw error;
+    }
+    
     return this.api.get(`/networks/json?lang=${lang}&index=${index}`);
   }
 
-  getScopes(lang, index) {
+  async getScopes(lang, index) {
+
+    try {
+      const response = await this.api.get(`/scopes/json?lang=${lang}&index=${index}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+      throw error;
+    }
+    
     return this.api.get(`/scopes/json?lang=${lang}&index=${index}`);
   }
 
-  getStationSettings(lang) {
+  async getStationSettings(lang) {
+
+    try {
+      const response = await this.api.get(`/stationsettings/json?lang=${lang}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stationsettings:", error);
+      throw error;
+    }
+    
     return this.api.get(`/stationsettings/json?lang=${lang}`);
   }
 
-  getStationsTypes(lang) {
+  async getStationsTypes(lang) {
+
+    try {
+      const response = await this.api.get(`/stationtypes/json?lang=${lang}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stationtypes:", error);
+      throw error;
+    }
+    
     return this.api.get(`/stationtypes/json?lang=${lang}`);
   }
 
-  getTransgressionTypes(lang) {
+  async getTransgressionTypes(lang) {
+
+    try {
+      const response = await this.api.get(`/transgressiontypes/json?lang=${lang}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching transgressiontypes:", error);
+      throw error;
+    }
+    
     return this.api.get(`/transgressiontypes/json?lang=${lang}`);
   }
 
-  getMeta(use, lang, dateFrom, dateTo, timeFrom, timeTo) {
-    return this.api.get(
-      `/meta/json?use=${use}&lang=${lang}&date_from=${dateFrom}&date_to=${dateTo}&time_from=${timeFrom}&time_to=${timeTo}`
-    );
+  async getMeta(use, lang, dateFrom, dateTo, timeFrom, timeTo) {
+
+    try {
+      const response = await this.api.get(`/meta/json?use=${use}&lang=${lang}&date_from=${dateFrom}&date_to=${dateTo}&time_from=${timeFrom}&time_to=${timeTo}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+      throw error;
+    }
+    
+    return this.api.get(`/meta/json?use=${use}&lang=${lang}&date_from=${dateFrom}&date_to=${dateTo}&time_from=${timeFrom}&time_to=${timeTo}`);
   }
 
-  getAirquality(dateFrom, dateTo, timeFrom, timeTo) {
-    return this.api.get(
-      `/airquality/json?date_from=${dateFrom}&time_from=${timeFrom}&date_to=${dateTo}&time_to=${timeTo}`
-    );
+  async getAirquality(dateFrom, dateTo, timeFrom, timeTo) {
+
+    try {
+      const response = await this.api.get(`/airquality/json?date_from=${dateFrom}&time_from=${timeFrom}&date_to=${dateTo}&time_to=${timeTo}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+      throw error;
+    }
+    
+    return this.api.get(`/airquality/json?date_from=${dateFrom}&time_from=${timeFrom}&date_to=${dateTo}&time_to=${timeTo}`);
   }
 
-  getStationUpTime() {
+  async getStationUpTime() {
+
+    try {
+      const response = await this.api.get(`/airquality/limits`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+      throw error;
+    }
+    
     return this.api.get(`/airquality/limits`);
   }
 
-  getMeasure(dateFrom, dateTo, timeFrom, timeTo, stationId, component, scope) {
-    return this.api.get(
-      `/measures/json?date_from=${dateFrom}&time_from=${timeFrom}&date_to=${dateTo}&time_to=${timeTo}&station=${stationId}&component=${component}&scope=${scope}`
-    );
+  async getMeasure(dateFrom, dateTo, timeFrom, timeTo, stationId, component, scope) {
+
+    try {
+      const response = await this.api.get(`/measures/json?date_from=${dateFrom}&time_from=${timeFrom}&date_to=${dateTo}&time_to=${timeTo}&station=${stationId}&component=${component}&scope=${scope}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+      throw error;
+    }
+    
+    return this.api.get(`/measures/json?date_from=${dateFrom}&time_from=${timeFrom}&date_to=${dateTo}&time_to=${timeTo}&station=${stationId}&component=${component}&scope=${scope}`);
   }
 
-  getMeasureLimits() {
+  async getMeasureLimits() {
+
+    try {
+      const response = await this.api.get(`/measures/limits`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+      throw error;
+    }
+    
     return this.api.get(`/measures/limits`);
   }
 
-  getTransgressions(year, lang, index) {
-    return this.api.get(
-      `/transgressions/json?component=1&year=${year}&lang=${lang}&index=${index}` //transgressions/json?component=1&year=2019&lang=de&index=code"
-    );
+  async getTransgressions(year, lang, index) {
+
+    try {
+      const response = await this.api.get(`/transgressions/json?component=1&year=${year}&lang=${lang}&index=${index}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching stations:", error);
+      throw error;
+    }
+    
+    return this.api.get(`/transgressions/json?component=1&year=${year}&lang=${lang}&index=${index}` /* transgressions/json?component=1&year=2019&lang=de&index=code"*/);
   }
 
+  /*
   getAnnualBalances(year, lang, index) {
-    return this.api.get(
-      `/annualbalances/json?component=1&year=${year}&lang=${lang}&index=${index}`
-    );
+    return this.api.get(`/api/annualbalances/json?component=1&year=${year}&lang=${lang}&index=${index}`);
+  }
+  */
+  async getAnnualBalances(year, lang, index) {
+    try {
+      const response = await this.api.get(`/annualbalances/json?component=1&year=${year}&lang=${lang}&index=${index}`, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching annual balances:", error);
+      throw error;
+    }
   }
 }
 
